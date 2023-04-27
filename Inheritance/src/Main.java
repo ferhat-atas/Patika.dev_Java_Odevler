02c4adafb79e33d93a54c898ae55aae6891f4d98
FERHAT ATAŞ <93721598+ferhat-atas@users.noreply.github.com>
2023-04-27 17:58:11 +0300

    Update Main.java

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -11,7 +11,8 @@ public class Main {
         
         
         OgretimGorevlisi o1 = new OgretimGorevlisi("Matematik", "Matematik", "Prof", "Hamza Ataş", "68806657", "fkshkfs@sfjsdf");
-        Akademisyen a1 = new Akademisyen("Fizik", "doc", "Ferhat Ataş", "0900 000 00 00", "gelme@com.tr");
+       // Akademisyen a1 = new Akademisyen("Fizik", "doc", "Ferhat Ataş", "0900 000 00 00", "gelme@com.tr"); 
+       //Akasemisyen abstract bir sınıf olduğu için artık nesne üretemiyoruz. Bize bir klavuz şablon gibi yardımcı oluyor.
         //o1.DerseGir();
         //o1.SenatoToplantisi();
         //o1.yemekhane();
@@ -19,7 +20,7 @@ public class Main {
         //o1.giriş("9", "17");
         //a1.giris();
         
-        Calisan[] girisYapanlar = {c1,o1,a1};
+        Calisan[] girisYapanlar = {c1,o1};
         c1.girisYapanlar(girisYapanlar);
     }
     
