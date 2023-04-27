3b809b02733e8b9c8efc7ca600146dcc8e424e88
FERHAT ATAŞ <93721598+ferhat-atas@users.noreply.github.com>
2023-04-27 16:54:41 +0300

    Update Main.java

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -8,17 +8,19 @@ public class Main {
        
         Calisan c1 = new Calisan("Ferhat Ataş", "0500 000 00 00", "non@gmail.com");
         
-        c1.giris();
-        c1.cıkıs();
+        
         
         OgretimGorevlisi o1 = new OgretimGorevlisi("Matematik", "Matematik", "Prof", "Hamza Ataş", "68806657", "fkshkfs@sfjsdf");
-        
+        Akademisyen a1 = new Akademisyen("Fizik", "doc", "Ferhat Ataş", "0900 000 00 00", "gelme@com.tr");
         //o1.DerseGir();
         //o1.SenatoToplantisi();
         //o1.yemekhane();
-        o1.giris("13");
-        o1.giriş("9", "17");
-        o1.giris();
+        //c1.giris("13");
+        //o1.giriş("9", "17");
+        //a1.giris();
+        
+        Calisan[] girisYapanlar = {c1,o1,a1};
+        c1.girisYapanlar(girisYapanlar);
     }
     
 }
