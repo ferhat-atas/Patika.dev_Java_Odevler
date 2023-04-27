73d19b33e558ffdbd3fa17da6098d9468467e226
FERHAT ATAŞ <93721598+ferhat-atas@users.noreply.github.com>
2023-04-27 17:55:19 +0300

    Update BilgiIslem.java

--- Inheritance/src/BilgiIslem.java
+++ Inheritance/src/BilgiIslem.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author FERHAT ATAŞ
- */
 public class BilgiIslem extends Memur {
     
     private String gorev;
@@ -29,6 +20,10 @@ public class BilgiIslem extends Memur {
     
     public void networkKurulum(){
         System.out.println(this.getAdSoyad()+" newtwork kurulumu yaptı.");
+    }
+    @Override
+    public void calıs(){
+         System.out.println(this.getAdSoyad()+" "+this.getDepartman()+" departmanında çalışmaya başladı.");
     }
     
     
